32cb77554aaca41c34efea87bd9267ae0f1ff43f
SensitiveWebUser <55949728+SensitiveWebUser@users.noreply.github.com>
2023-02-04 18:01:41 +0000

    init

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { SerialPort, ReadlineParser } from 'serialport';
+import { Server as socket } from 'socket.io';
+
+// App setup
+var app = express();
+
+const port = new SerialPort({
+	path: 'COM5',
+	baudRate: 9600,
+});
+
+var server = app.listen(8888, function () {
+	console.log('listening for requests on port 8888,');
+});
+
+let io = new socket(server, { cors: { origin: '*' } });
+io.on('connection', function (socket) {
+	console.log(`${socket.id} is connected`);
+
+	socket.on('serialdata', function (data) {
+		console.log(data);
+		port.write(data);
+	});
+
+	const parser = port.pipe(new ReadlineParser());
+	parser.read;
+});
