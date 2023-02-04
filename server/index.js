459cd4eab3bd5f9a90be59c032daa5ce95d2c3e8
Alexander Hornsby <alexahornsby@hotmail.co.uk>
2023-02-04 21:48:11 +0000

    updated server checks OS running

--- server/index.js
+++ server/index.js
@@ -1,17 +1,55 @@
 import express from "express";
-import { SerialPort, ReadlineParser } from "serialport";
+import { SerialPort } from "serialport";
 import { Server as socket } from "socket.io";
 
 // App setup
 var app = express();
 
+var isWin = () => {
+  console.log(`System that is running is ${process.platform}`);
+  return process.platform == "win64";
+};
+
+const isWired = true;
+
+const serverPort = 8888;
+
+const WINDOWS_XBEE_PATH = "COM10";
+const LINUX_XBEE_PATH = "/dev/ttyUSB0";
+
+const WINDOWS_CABLE_PATH = "COM5";
+const LINUX_CABLE_PATH = "/dev/ttyUSB0";
+
+const path = () => {
+  var path = "invalide system";
+  var option = [];
+
+  if (isWin()) {
+    option = [WINDOWS_CABLE_PATH, WINDOWS_XBEE_PATH];
+  } else {
+    option = [LINUX_CABLE_PATH, LINUX_XBEE_PATH];
+  }
+
+  console.log(option);
+
+  if (option.length > 0 && isWired) {
+    path = option[0];
+  } else if (option.length > 0) {
+    path = option[1];
+  }
+
+  console.log(`choosen serial path is: ${path}`);
+
+  return path;
+};
+
 const port = new SerialPort({
-  path: "/dev/ttyACM0",
+  path: path(),
   baudRate: 9600,
 });
 
-var server = app.listen(8888, function () {
-  console.log("listening for requests on port 8888,");
+var server = app.listen(serverPort, function () {
+  console.log(`listening for requests on port ${serverPort},`);
 });
 
 let io = new socket(server, { cors: { origin: "*" } });
@@ -22,7 +60,4 @@ io.on("connection", function (socket) {
     console.log(data);
     port.write(data);
   });
-
-  const parser = port.pipe(new ReadlineParser());
-  parser.read;
 });
