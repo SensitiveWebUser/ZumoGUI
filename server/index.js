110b96fdd888ffc0f5025c1a8ec1fb3ed44a5d1a
Alexander Hornsby <alexahornsby@hotmail.co.uk>
2023-02-04 20:17:01 +0000

    Added new button maps for controller

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-import express from 'express';
-import { SerialPort, ReadlineParser } from 'serialport';
-import { Server as socket } from 'socket.io';
+import express from "express";
+import { SerialPort, ReadlineParser } from "serialport";
+import { Server as socket } from "socket.io";
 
 // App setup
 var app = express();
 
 const port = new SerialPort({
-	path: 'COM5',
-	baudRate: 9600,
+  path: "/dev/ttyACM0",
+  baudRate: 9600,
 });
 
 var server = app.listen(8888, function () {
-	console.log('listening for requests on port 8888,');
+  console.log("listening for requests on port 8888,");
 });
 
-let io = new socket(server, { cors: { origin: '*' } });
-io.on('connection', function (socket) {
-	console.log(`${socket.id} is connected`);
+let io = new socket(server, { cors: { origin: "*" } });
+io.on("connection", function (socket) {
+  console.log(`${socket.id} is connected`);
 
-	socket.on('serialdata', function (data) {
-		console.log(data);
-		port.write(data);
-	});
+  socket.on("serialdata", function (data) {
+    console.log(data);
+    port.write(data);
+  });
 
-	const parser = port.pipe(new ReadlineParser());
-	parser.read;
+  const parser = port.pipe(new ReadlineParser());
+  parser.read;
 });
