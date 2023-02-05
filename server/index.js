bfd296f7482a6e829f3414f7f8ee85f9f6a98eca
SensitiveWebUser <Alexahornsby@hotmail.co.uk>
2023-02-05 09:39:30 +0000

    optimised server side

--- server/index.js
+++ server/index.js
@@ -1,63 +1,50 @@
 import express from 'express';
 import { SerialPort } from 'serialport';
-import { Server as socket } from 'socket.io';
+import { Server as SocketIO } from 'socket.io';
 
-// App setup
-var app = express();
-
-var isWin = () => {
-	console.log(`System that is running is ${process.platform}`);
-	return process.platform == 'win32';
-};
+const app = express();
+const isWin = () => process.platform === 'win32';
 
 const isWired = true;
 
 const serverPort = 8888;
 
-const WINDOWS_XBEE_PATH = 'COM10';
-const LINUX_XBEE_PATH = '/dev/ttyUSB0';
-
-const WINDOWS_CABLE_PATH = 'COM5';
-const LINUX_CABLE_PATH = '/dev/ttyUSB0';
-
-const path = () => {
-	var path = 'invalide system';
-	var option = [];
-
-	if (isWin()) {
-		option = [WINDOWS_CABLE_PATH, WINDOWS_XBEE_PATH];
-	} else {
-		option = [LINUX_CABLE_PATH, LINUX_XBEE_PATH];
-	}
-
-	console.log(option);
-
-	if (option.length > 0 && isWired) {
-		path = option[0];
-	} else if (option.length > 0) {
-		path = option[1];
-	}
-
-	console.log(`choosen serial path is: ${path}`);
-
-	return path;
+const paths = {
+	windows: [
+		{ type: 'cable', path: 'COM5' },
+		{ type: 'xbee', path: 'COM10' },
+	],
+	linux: [
+		{ type: 'cable', path: '/dev/ttyUSB0' },
+		{ type: 'xbee', path: '/dev/ttyUSB0' },
+	],
 };
+const selectedPath = paths[isWin() ? 'windows' : 'linux'].find(
+	(p) => p.type === (isWired ? 'cable' : 'xbee')
+).path;
 
-const port = new SerialPort({
-	path: path(),
+const serialPort = new SerialPort({
+	path: selectedPath,
 	baudRate: 9600,
 });
 
-var server = app.listen(serverPort, function () {
-	console.log(`listening for requests on port ${serverPort},`);
-});
+const server = app.listen(serverPort, () =>
+	console.log(`Listening for requests on port ${serverPort}.`)
+);
+const io = new SocketIO(server, { cors: { origin: '*' } });
 
-let io = new socket(server, { cors: { origin: '*' } });
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
 	console.log(`${socket.id} is connected`);
 
-	socket.on('serialdata', function (data) {
+	socket.on('serialdata', (data) => {
 		console.log(data);
-		port.write(data + '\n');
+		serialPort.write(`${data}\n`);
+	});
+});
+
+serialPort.on('open', function () {
+	console.log('-- Connection opened --');
+	serialPort.on('data', function (data) {
+		console.log('Data received: ' + data);
 	});
 });
