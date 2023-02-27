e6f42ab8bacdf10e964c1e4a437270fd07d12d33
SensitiveWebUser <55949728+SensitiveWebUser@users.noreply.github.com>
2023-02-27 07:56:53 +0000

    Added new drawing canvas and setup proximity check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Gamepad from 'react-gamepad';
 import { SocketContext } from './context/socket';
 
-import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
 import Typography from '@mui/material/Typography';
+
+import ControlButton from './components/ControlButton';
+import Canvas from './components/Canvas';
 
 import Filter1Icon from '@mui/icons-material/Filter1';
 import Filter2Icon from '@mui/icons-material/Filter2';
 import Filter3Icon from '@mui/icons-material/Filter3';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import RedoIcon from '@mui/icons-material/Redo';
 import StopIcon from '@mui/icons-material/Stop';
 import UndoIcon from '@mui/icons-material/Undo';
+
+// Used to map the gamepad buttons to serial data sent to the Arduino
 const actions = [
 	{ key: 'left', icon: UndoIcon, label: 'Left' },
 	{ key: 'right', icon: RedoIcon, label: 'Right' },
@@ -23,24 +29,40 @@ const actions = [
 	{ key: 'accelerate', icon: KeyboardArrowUpIcon, label: 'Accelerate' },
 	{ key: 'decelerate', icon: KeyboardArrowDownIcon, label: 'Decelerate' },
 	{ key: 'stop', icon: StopIcon, label: 'Stop' },
-	{ key: 'switch', icon: PowerSettingsNewIcon, label: 'switch' },
 	{ key: 'mode1', icon: Filter1Icon, label: 'mode 1' },
 	{ key: 'mode2', icon: Filter2Icon, label: 'mode 2' },
 	{ key: 'mode3', icon: Filter3Icon, label: 'mode 3' },
-	{ key: 'switchMode', icon: Filter3Icon, label: 'switchMode' },
 ];
 
 const App = () => {
+	const [hasBoxlayout, setHasBoxlayout] = useState(false);
+	const [color, setColor] = useState('white');
+	const [value, setValue] = useState(1);
+	const [direction, setDirection] = useState('stop');
+
 	const socket = useContext(SocketContext);
 	const emitSerialData = (key) => socket.emit('serialdata', key);
 
 	return (
-		<div>
+		<Box>
 			<Typography variant="h3">Arduino Zumo Control</Typography>
 			<Typography variant="subtitle1">
 				Click the buttons to send data to the serial port. This setup uses a
 				React client and a Node server to communicate with an Arduino Zumo Bot.
 			</Typography>
+
+			<Typography pt={2} variant="h4">
+				Controls
+			</Typography>
+
+			<Typography variant="subtitle1">
+				<Checkbox
+					checked={hasBoxlayout}
+					onChange={() => setHasBoxlayout(!hasBoxlayout)}
+				/>
+				Box/Debug layout
+			</Typography>
+
 			<Gamepad
 				onA={() => emitSerialData('mode1')}
 				onB={() => emitSerialData('right')}
@@ -59,23 +81,197 @@ const App = () => {
 				onRB={() => emitSerialData('accelerate')}
 				onLS={() => emitSerialData('decelerate')}
 			>
-				<Grid container spacing={1}>
-					{actions.map(({ key, icon: Icon, label }, index) => (
-						<Grid key={key} item xs={index < 3 ? 4 : 6} md={2}>
-							<Button
-								variant="contained"
-								color="primary"
-								fullWidth
-								onClick={() => emitSerialData(key)}
-							>
-								<Icon fontSize="large" />
-								{label}
-							</Button>
+				<Box>
+					{hasBoxlayout && (
+						<Grid container spacing={1}>
+							{actions.map(({ key, icon: Icon, label }, index) => (
+								<Grid key={key} item xs={index < 3 ? 4 : 6} md={2}>
+									<ControlButton
+										icon={Icon}
+										text={label}
+										onClick={() => emitSerialData(key)}
+									/>
+								</Grid>
+							))}
 						</Grid>
-					))}
-				</Grid>
+					)}
+
+					<Grid
+						container
+						spacing={2}
+						mt={2}
+						alignItems="center"
+						justifyContent="center"
+					>
+						<Grid item xs={4} md={2}>
+							<ControlButton
+								icon={Filter1Icon}
+								text="Mode 1"
+								onClick={() => emitSerialData('Mode1')}
+							/>
+						</Grid>
+						<Grid item xs={4} md={2}>
+							<ControlButton
+								icon={Filter2Icon}
+								text="Mode 2"
+								onClick={() => emitSerialData('Mode2')}
+							/>
+						</Grid>
+						<Grid item xs={4} md={2}>
+							<ControlButton
+								icon={Filter3Icon}
+								text="Mode 3"
+								onClick={() => emitSerialData('Mode3')}
+							/>
+						</Grid>
+					</Grid>
+
+					<Grid
+						container
+						spacing={2}
+						mt={2}
+						alignItems="center"
+						justifyContent="center"
+					>
+						<Grid item xs={4} md={2}>
+							<ControlButton
+								icon={KeyboardArrowUpIcon}
+								text="Accelerate"
+								onClick={() => emitSerialData('Accelerate')}
+							/>
+						</Grid>
+						<Grid item xs={4} md={2}>
+							<ControlButton
+								icon={KeyboardArrowDownIcon}
+								text="Decelerate"
+								onClick={() => emitSerialData('Decelerate')}
+							/>
+						</Grid>
+					</Grid>
+
+					<Grid
+						container
+						spacing={2}
+						mt={2}
+						alignItems="center"
+						justifyContent="center"
+					>
+						<Grid item xs={4} md={2}>
+							<ControlButton
+								icon={UndoIcon}
+								text="Left"
+								onClick={() => emitSerialData('Left')}
+							/>
+						</Grid>
+						<Grid item xs={4} md={2}>
+							<Box mb={1}>
+								<ControlButton
+									icon={KeyboardArrowUpIcon}
+									text="Forward"
+									onClick={() => emitSerialData('Forward')}
+								/>
+							</Box>
+							<Box>
+								<ControlButton
+									icon={KeyboardArrowDownIcon}
+									text="Backward"
+									onClick={() => emitSerialData('Backward')}
+								/>
+							</Box>
+						</Grid>
+						<Grid item xs={4} md={2}>
+							<ControlButton
+								icon={RedoIcon}
+								text="Right"
+								onClick={() => emitSerialData('Right')}
+							/>
+						</Grid>
+					</Grid>
+					<Box mt={2} mx={6}>
+						<ControlButton
+							icon={StopIcon}
+							text="Stop"
+							onClick={() => emitSerialData('Stop')}
+						/>
+					</Box>
+
+					{hasBoxlayout && (
+						<Box mt={2} justifyItems={'center'}>
+							<Box maxWidth={600} mx="auto">
+								<Typography pt={2} variant="h4">
+									Drawer Debug Controls
+								</Typography>
+								<Box
+									mt={2}
+									display="flex"
+									flexDirection="column"
+									alignItems="stretch"
+								>
+									<Box mb={1}>
+										<Button
+											variant="contained"
+											color="primary"
+											fullWidth
+											onClick={() =>
+												setDirection(direction === 'left' ? 'right' : 'left')
+											}
+										>
+											{direction === 'left' ? 'Turn Right' : 'Turn Left'}
+										</Button>
+									</Box>
+									<Box mb={1}>
+										<Button
+											variant="contained"
+											color="primary"
+											fullWidth
+											onClick={() =>
+												setDirection(direction === 'up' ? 'down' : 'up')
+											}
+										>
+											{direction === 'up' ? 'Move Down' : 'Move Up'}
+										</Button>
+									</Box>
+									<Box mb={1}>
+										<Button
+											variant="contained"
+											color="primary"
+											fullWidth
+											onClick={() => setDirection('point')}
+										>
+											point
+										</Button>
+									</Box>
+									<Box mb={1}>
+										<Button
+											variant="contained"
+											color="primary"
+											fullWidth
+											onClick={() => setDirection('stop')}
+										>
+											stop
+										</Button>
+									</Box>
+									<Box mb={1}>
+										<Button
+											variant="contained"
+											color="primary"
+											fullWidth
+											onClick={() => setDirection('clear')}
+										>
+											clear
+										</Button>
+									</Box>
+								</Box>
+							</Box>
+						</Box>
+					)}
+
+					<Box mt={2} mx={6}>
+						<Canvas color={color} speed={value} direction={direction} />
+					</Box>
+				</Box>
 			</Gamepad>
-		</div>
+		</Box>
 	);
 };
 
---
+++ client/src/components/Canvas.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+
+const Canvas = (props) => {
+	const { color, speed, direction } = props;
+
+	const value = speed / 100;
+
+	const size = 500;
+
+	// Create a ref for the stop flag
+	const stopRef = React.useRef(false);
+
+	// Get a reference to the canvas element
+	const canvasRef = React.useRef(null);
+
+	// Create a state for storing the previous position
+	const [prevPos, setPrevPos] = useState({
+		x: size / 2,
+		y: size / 2,
+	});
+
+	// Create a state for storing the previous direction
+	const [prevDir, setPrevDir] = useState(direction);
+
+	// Update the canvas context on each render
+	useEffect(() => {
+		// Get the canvas context
+		const ctx = canvasRef.current.getContext('2d');
+
+		// Clear the previous drawing
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+		// Set an initial position for drawing lines
+		let x = prevPos.x;
+		let y = prevPos.y;
+
+		// Define a function that draws a line on the canvas context
+		const drawLine = () => {
+			// Check if stop flag is true and return if so
+			if (stopRef.current) return;
+
+			// Draw a line with the current color and value only if direction has not changed
+			if (direction === prevDir) {
+				ctx.strokeStyle = color;
+				ctx.lineWidth = 3;
+				ctx.beginPath();
+				ctx.moveTo(x - value / 2, y - value / 2);
+				ctx.lineTo(x + value / 2, y + value / 2);
+				ctx.stroke();
+			}
+
+			// Increment x or y depending on direction state
+			switch (direction) {
+				case 'up':
+					y -= value;
+					break;
+				case 'down':
+					y += value;
+					break;
+				case 'left':
+					x -= value;
+					break;
+				case 'right':
+					x += value;
+					break;
+				case 'point':
+					ctx.arc(x, y, 3, 0, 2 * Math.PI);
+					ctx.stroke();
+					break;
+				case 'clear':
+					ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+					break;
+				default:
+					break;
+			}
+
+			// If direction is 'point', update the previous direction state to the last non-'point' direction
+			if (direction === 'point') {
+				setPrevDir(prevDir !== 'point' ? prevDir : 'right');
+			}
+
+			// Wrap around if x or y goes out of bounds
+			if (x < value || x > ctx.canvas.width - value) {
+				x = Math.abs(ctx.canvas.width - x);
+			}
+			if (y < value || y > ctx.canvas.height - value) {
+				y = Math.abs(ctx.canvas.height - y);
+			}
+
+			// Update the previous position and direction states
+			if (direction !== 'clear') {
+				setPrevPos({ x, y });
+			} else {
+				setPrevPos({
+					x: size / 2,
+					y: size / 2,
+				});
+			}
+			setPrevDir(direction);
+
+			// Request an animation frame
+			requestAnimationFrame(drawLine);
+		};
+
+		// Call the drawLine function once
+		drawLine();
+	}, [color, value, direction, prevPos.x, prevPos.y, prevDir]); // Run only when color,value or direction changes
+
+	// Return a canvas element with a ref
+
+	return <canvas ref={canvasRef} width={size} height={size} />;
+};
+
+export default Canvas;
